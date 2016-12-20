d6f836eab8d9c75a60df21116a1b584a2094463e
jacknbeans <jack.reggin@gmail.com>
2016-12-20 15:05:06 +0100

    Changed from amd64 to x64

--- code/build.bat
+++ code/build.bat
@@ -2,6 +2,6 @@
 
 mkdir ..\build
 pushd ..\build
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64
 cl -DHANDMADE_WIN32=1 -FC -Zi ..\code\win32_handmade.cpp user32.lib gdi32.lib
 popd
