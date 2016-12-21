0a468361ec5e64ad9a96e375e93f082514455645
jacknbeans <jack.reggin@gmail.com>
2016-12-21 09:55:57 +0100

    Centralized defines and win32 structs.

---
+++ code/win32_handmade.h
@@ -0,0 +1,29 @@
+#if !defined(WIN32_HANDMADE_H)
+
+#include "defines.h"
+#include <windows.h>
+
+struct Win32OffscreenBuffer {
+  BITMAPINFO info;
+  void *memory;
+  int width;
+  int height;
+  int pitch;
+};
+
+struct Win32WindowDimension {
+  int width;
+  int height;
+};
+
+struct Win32SoundOutput {
+  int samplesPerSecond;
+  uint32 runningSampleIndex;
+  int bytesPerSample;
+  int secondaryBufferSize;
+  real32 tSine;
+  int latencySampleCount;
+};
+
+#define WIN32_HANDMADE_H
+#endif
